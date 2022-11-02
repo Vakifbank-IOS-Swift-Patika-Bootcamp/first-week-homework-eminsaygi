1df9ba2cf04a54218ba9fc229dcbf7adac00edfb
Emin Saygı <emin_0001@hotmail.com>
2022-11-02 16:43:02 +0300

    Homework1 added

---
+++ HomeWork.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//MARK: - Ödev1
+//func Palindrome(str: String) -> Bool {
+//    var str = str
+//    var str2 = ""
+//    for i in str {
+//        str2 = "\(i)" + str2
+//    }
+//    if str == str2 {
+//        return true
+//    } else {
+//        return false
+//    }
+//}
+//Palindrome(str: "ey edip adanada pide ye")
+
+
